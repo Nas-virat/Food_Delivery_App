10bb388a6b0951b488dff62236a7326b262f51aa
Nas-virat <Napasvinitnantharat@gmail.com>
2023-03-15 14:31:18 +0700

    add page

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -12,7 +12,7 @@ class ApiClient extends GetConnect implements GetxService {
 
     _mainHeaders = {
       'Content-type': 'application/json; charset=UTF-8',
-      'Authorization': 'Bearer $token',
+      //'Authorization': 'Bearer $token',
     };
   }
 
